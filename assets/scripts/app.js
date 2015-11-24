3a570c0c1829a3bd6b1877aebbffb9490cb60137
Ian Smith <iansmith1026@gmail.com>
2015-11-23 17:36:15 -0700

    wrote functions to handle brewery info click events, started working on printing out information

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,13 @@
 
 // Google maps api key = AIzaSyAu9tqAfwr9y_b4MUrI_Sg8iMbfIDe24Z0
 
-var lat, long, coordArr, info, printArr, beerArr;
+var lat, long, coordArr, info, printArr, beerArr, brewAssets;
 var markers = [];
 var $total = $('.brew-total');
 var $list = $('.brew-list');
 var $beerMe = $('button');
+var $breweryImg = $('.brewery-img');
+var $breweryDescription = $('.brewery-description');
 
 $beerMe.on('click', function(event) {
   event.preventDefault();
@@ -33,28 +35,28 @@ function getListings(city, state) {
   $.get(search, function(object) {
     if (object.numberOfPages === 1) {
       brewArr = object.data;
-      extractInfo(brewArr);
+      extractBreweryInfo(brewArr);
     } else if (object.numberOfPages === 2) {
       brewArr = object.data;
       $.get(search + '%26p%3D2', function(object2) {
         brewArr = brewArr.concat(object2.data);
-        extractBrewInfo(brewArr);
+        extractBreweryInfo(brewArr);
       });
     }
   });
 }
 
-function extractBrewInfo(arr) {
+function extractBreweryInfo(arr) {
   var length = arr.length;
   $total.append($('<h4>').text('Listings (' + length + ')'));
   for (var i = 0; i < length; i++) {
     var brewName = arr[i].brewery.name;
-    var brewID = arr[i].id;
+    var breweryID = arr[i].brewery.id;
     var type = arr[i].locationTypeDisplay;
     var address = arr[i].streetAddress;
     var zip = arr[i].postalCode;
     var phone = arr[i].phone;
-    var breweryInfo = [brewName, brewID, type, address, zip, phone];
+    var breweryInfo = [brewName, breweryID, type, address, zip, phone];
     printArr.push(breweryInfo);
     var brewLat = arr[i].latitude;
     var brewLong = arr[i].longitude;
@@ -68,11 +70,15 @@ function printInfo(arr) {
   var length = arr.length;
   for (var i = 0; i < length; i++){
     $list.append($('<hr>'));
-    $list.append($('<h3>', {
+    var h3 = $('<h3>', {
       text: arr[i][0],
       class: 'brew-search',
       id: arr[i][1]
-    }));
+    });
+    h3.on('click', function(event){
+      getBreweryInfoAndImg(event.target.id);
+    });
+    $list.append(h3);
     $list.append($('<h5>').text(arr[i][2]));
     $list.append($('<p>').html(arr[i][3] + ' ' + arr[i][4] + '<br/>' + arr[i][5]));
   }
@@ -87,15 +93,31 @@ function printInfo(arr) {
 // %26 is &
 // %3F is ?
 
+// Odells ID = rQkKIB
+
 function getBreweryInfoAndImg(brewID) {
+  brewAssets = [];
   var brewDescription = 'https://jsonp.afeld.me/?url=http%3A%2F%2Fapi.brewerydb.com%2Fv2%2Fbrewery%2F' + brewID + '%3Fkey%3D0d28b6999d59c70e170fb29165a647d2';
   $.get(brewDescription, function(object) {
     var description = object.data.description;
     var icon = object.data.images.icon;
     var medPic = object.data.images.medium;
     var lrgPic = object.data.images.large;
+    brewAssets.push(description);
+    brewAssets.push(lrgPic);
   });
+  printAssets(brewAssets);
   getBeers(brewID);
+}
+
+function printAssets(arr) {
+  $breweryDescription.append($('<h3>').text('Description'));
+  $breweryDescription.append($('<p>').text(arr[0]));
+  $breweryImg.append($('<h3>').text('Brewery'));
+  // $breweryImg.append($('<img>', {
+  //   class: 'img-responsive',
+  //   src: arr[1]
+  // }));
 }
 
 function getBeers(brewID) {
@@ -105,8 +127,6 @@ function getBeers(brewID) {
     extractBeerInfo(beerArr);
   });
 }
-
-getBeers('rQkKIB');
 
 function extractBeerInfo(arr) {
   var length = arr.length;
